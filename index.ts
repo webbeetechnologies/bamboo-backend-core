7ea3e23d6c6a5304b029a0966b601194be7a8755
Tobias <github@mythli.net>
2022-12-31 11:28:23 +0300

    yeah

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 let moneys = 100;
 
 export const yolo = (lifeEnjoyment: number) => {
-    throw new Error('Not implemented');
+    throw new Error('yolo');
 
     return lifeEnjoyment >= 100 ? 'yolo' : 'no yolo';
 };
