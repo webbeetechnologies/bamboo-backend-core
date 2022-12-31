995b69dd2b174ec2241971f9bf7031ca81e8d9ed
Tobias <github@mythli.net>
2022-12-31 13:29:21 +0300

    introduced gts for linting

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 let moneys = 100;
 
 export const yolo = (lifeEnjoyment: number) => {
-  return lifeEnjoyment >= 100 ? "yolo" : "no yolo";
+  return lifeEnjoyment >= 100 ? 'yolo' : 'no yolo';
 };
 
 export const getLoan = (amount: number) => {
   if (amount > moneys) {
-    throw new Error("You are too poor to get a loan");
+    throw new Error('You are too poor to get a loan');
   }
 
   moneys -= amount;
