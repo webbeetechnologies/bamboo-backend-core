bc7930ad103e29b4a9d05cdf6fb8c3115b33268c
Tobias <github@mythli.net>
2022-12-31 12:27:03 +0300

    check6

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 let moneys = 100;
 
 export const yolo = (lifeEnjoyment: number) => {
-  throw new Error("check5");
+  throw new Error("check6");
 
   return lifeEnjoyment >= 100 ? "yolo" : "no yolo";
 };
